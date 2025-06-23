0870914550caf5c46104812044b0113512a6c425
Guilherme <guilhermerochabastos@hotmail.com>
2025-06-22 23:10:37 -0300

    feat(backend): Configuração de banco de dados e intalação de dependencias

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const connectDB = require('./config/dbConfig');
+connectDB();
 
 app.use(express.json());
+
 
 app.get('/', (req, res) => {
   res.send('Servidor Express rodando...');
